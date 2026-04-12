c63a68ce7269ed0b4b989941bc50e8c839a3640c
Habib Mukhlis Triatmojo <89024106+Htech5@users.noreply.github.com>
2026-04-13 02:06:55 +0700

    Update MagazineFlipbookBook.js

--- src/components/MagazineFlipbookBook.js
+++ src/components/MagazineFlipbookBook.js
@@ -1,9 +1,18 @@
 "use client";
 
 export default function MagazineFlipbookBook({ pdfUrl }) {
+  if (!pdfUrl) {
+    return (
+      <div className="mag-viewer__book-wrap">
+        <p className="mag-viewer__status">PDF tidak tersedia.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="mag-viewer__book-wrap">
       <div
+        className="mag-viewer__iframe-wrap"
         style={{
           width: "100%",
           height: "80vh",
